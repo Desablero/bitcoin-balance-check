61125429ff106b2f2dc75de80014f14775cc7e25
Ihor Kupa <ihorkupa@MacBook-Air-Ihor-Kupa.local>
2021-03-07 01:09:51 +0200

    Базовая структура проекта
    * Удалены лишние файлы
    * Добавлены компонентные папки app, address-card

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+import './app.css'
+
+export default class App extends Component {
+
+    render(){
+
+        return(
+            <h1 className="main-title">BBC</h1>
+        )
+    }
+}
