e9c16a449819054ee7d91165e043992bf724013b
Ihor Kupa <ihorkupa@MacBook-Air-Ihor-Kupa.local>
2021-03-07 01:53:52 +0200

    Компонент card-address
    * Переименован компонент address-card на card-address
    * Созданы классы и тестовый-контент для card-address

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react'
 
 import './app.css'
+import CardAddress from '../card-address'
 
 export default class App extends Component {
+
+    state = {
+        addressList: ['1iKupaKz422mtpsPaAoagna1Zzgwk15wN']
+    }
 
     render(){
 
         return(
-            <h1 className="main-title">BBC</h1>
+            <div>
+                <h1 className="main-title">BBC</h1>
+                <CardAddress />
+            </div>
         )
     }
 }
---
+++ src/components/card-address/index.js
@@ -0,0 +1,2 @@
+import CardAddress from './card-address'
+export default CardAddress
