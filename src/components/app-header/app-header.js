0427e996c388d0d4a1af4150fb12eb216166223f
Ihor Kupa <ihorkupa@MacBook-Air-Ihor-Kupa.local>
2021-03-08 03:01:48 +0200

    minor: App Title

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './app-header.css'
+
+export default function AppHeader () {
+    return (
+        <h1 className="app-header-title">Bitcoin Balance Cheker</h1>
+    )
+}
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import BTCAPI from '../../service/btc-api'
 
 import './app.css'
+import AppHeader from '../app-header'
 import CardList from '../cards-list'
 import AddAddress from '../add-address'
 
@@ -13,50 +14,50 @@ export default class App extends Component {
     btcApi = new BTCAPI()
 
     state = {
-        addressList: []   
+        addressList: []
     }
 
-    render(){
+    render() {
 
         const onAddAddress = (address) => {
             this.btcApi.getAddressInfo(address)
-            .then((addressInfo) => {
-                this.setState(({addressList}) => {
-                    const oldList = addressList
-                    const newList = [...oldList, addressInfo]
-                    return {
-                        addressList: newList
-                    }
+                .then((addressInfo) => {
+                    this.setState(({ addressList }) => {
+                        const oldList = addressList
+                        const newList = [...oldList, addressInfo]
+                        return {
+                            addressList: newList
+                        }
+                    })
                 })
-            })
-        }        
+        }
 
         const onDeleteAddress = (id) => {
-            this.setState(({addressList}) => {
+            this.setState(({ addressList }) => {
                 const idx = this.state.addressList.findIndex((ad) => {
                     return ad.id === id
-                }) 
+                })
                 console.log(idx);
                 const oldList = addressList
-                const newList = [...oldList.slice(0,idx), ...oldList.slice(idx+1)]
+                const newList = [...oldList.slice(0, idx), ...oldList.slice(idx + 1)]
                 return {
                     addressList: newList
                 }
             })
         }
 
-        const {addressList} = this.state
+        const { addressList } = this.state
         console.log(addressList);
 
-        return(
+        return (
             <div>
-                <h1 className="main-title">BBC</h1>
-                <AddAddress onAddAddress={onAddAddress}/>
+                <AppHeader />
+                <AddAddress onAddAddress={onAddAddress} />
                 <button onClick={() => onAddAddress('34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo')}>Add</button>
                 <CardList addressList={this.state.addressList}
-                          onDeleteAddress={onDeleteAddress}
-                    />
-                
+                    onDeleteAddress={onDeleteAddress}
+                />
+
             </div>
         )
     }
